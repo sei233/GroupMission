9b6a092a35314c331b6612c04a955828fab0f43a
spades <lihaiwinning@qq.com>
2018-08-17 17:37:41 +0800

    提交修改

--- src/com/market/bean/person.java
+++ src/com/market/bean/person.java
@@ -2,6 +2,6 @@ package com.market.bean;
 
 public class person {
     public static void main(String[] args) {
-        System.out.println("hello a ");
+        System.out.println("hello a nnn");
     }
 }
