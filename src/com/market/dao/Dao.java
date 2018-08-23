77ad872b39076a8020c796d31a4dd5b7dc3e0170
sei233 <1136420833@qq.com>
2018-08-23 08:19:20 +0800

    提交时间:2018/8/23 8:13:00 提交人:石乾 提交内容:修正dao

--- src/com/market/dao/Dao.java
+++ src/com/market/dao/Dao.java
@@ -63,10 +63,12 @@ public class Dao<T> {
     public int AddDeleteChange(String sql,Object... objects){
         Connection connection = getConnection();
         try {
+            int j=0;
             PreparedStatement smt = connection.prepareStatement(sql);
             if (objects != null && objects.length > 0) {
                 for (int i = 0; i < objects.length; i++) {
-                    smt.setObject(i+1,objects[i]);
+                    { smt.setObject(j+1,objects[i]);
+                      j++; }
                 }
             }
             return smt.executeUpdate();
